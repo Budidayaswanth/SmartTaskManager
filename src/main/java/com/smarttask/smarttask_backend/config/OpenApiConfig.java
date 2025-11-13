a063366003187bc9868d6777b3eef76773030458
YaswanthBudida <yaswanth.budida@intutr.com>
2025-11-13 10:06:06 +0530

    fixed all problems

--- src/main/java/com/smarttask/smarttask_backend/config/OpenApiConfig.java
+++ src/main/java/com/smarttask/smarttask_backend/config/OpenApiConfig.java
@@ -6,21 +6,27 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${swagger.server-url:}")
+    private String swaggerServerUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("SmartTask API")
                         .version("1.0")
                         .description("SmartTask Backend API documentation"))
-                .addServersItem(new Server().url("http://localhost:8080"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
@@ -29,5 +35,11 @@ public class OpenApiConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+
+        if (StringUtils.hasText(swaggerServerUrl)) {
+            openAPI.setServers(List.of(new Server().url(swaggerServerUrl)));
+        }
+
+        return openAPI;
     }
 }
